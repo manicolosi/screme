8d523e41d709e9e83a41ee56d76ed0d1fd5b9032
Mark A. Nicolosi <mark@nicolosi.me>
2010-03-06 02:48:38 -0600

    Allow nesting of quotation: '''a

--- lib/parser.rb
+++ lib/parser.rb
@@ -2,15 +2,14 @@ require 'treetop'
 
 module ExpressionNode
   def to_ast
-    if quote.is_a? QuoteNode
-      [:quote, expr.to_ast]
-    else
-      expr.to_ast
-    end
+    expr.to_ast
   end
 end
 
-module QuoteNode
+module QuotedNode
+  def to_ast
+    [:quote, expression.to_ast]
+  end
 end
 
 module ListNode
--- spec/quotation_spec.rb
+++ spec/quotation_spec.rb
@@ -2,23 +2,41 @@ require File.dirname(__FILE__) + '/../lib/screme.rb'
 
 describe "Quotation" do
   it "should be able to quote single atoms" do
-    interpreter = ScremeInterpreter.new
     input = '(quote a)'
+    evaluate(input).should == :a
+  end
 
-    interpreter.parse_and_eval(input).should == :a
+  it "should be able to quote single atoms (apostrophe)" do
+    input = "'a"
+    evaluate(input).should == :a
   end
 
   it "should be able to quote an entire list" do
-    interpreter = ScremeInterpreter.new
     input = '(quote (a 1 b 2 c 3))'
-
-    interpreter.parse_and_eval(input).should == [:a, 1, :b, 2, :c, 3]
+    evaluate(input).should == [:a, 1, :b, 2, :c, 3]
   end
 
-  it "should be able to use the apostrophe as a shortcut to quote" do
-    interpreter = ScremeInterpreter.new
+  it "should be able to quote an entire list (apostrophe)" do
     input = "'(a 1 b 2 c 3)"
+    evaluate(input).should == [:a, 1, :b, 2, :c, 3]
+  end
 
-    interpreter.parse_and_eval(input).should == [:a, 1, :b, 2, :c, 3]
+  it "should be able to quote a quoted expression" do
+    input = '(quote (quote a))'
+    evaluate(input).should == [:quote, :a]
+  end
+
+  it "should be able to quote a quoted expression (apostrophes)" do
+    input = "''a"
+    evaluate(input).should == [:quote, :a]
+  end
+
+  it "apostrophes can be nested deeply" do
+    input = "'''''a"
+    evaluate(input).should == [:quote, [:quote, [:quote, [:quote, :a]]]]
+  end
+
+  def evaluate(input)
+    ScremeInterpreter.new.parse_and_eval(input)
   end
 end
