e9d9eea99c3e42dc87955b95541c8dce4aa6f9bc
Mark A. Nicolosi <mark@nicolosi.me>
2010-03-08 14:04:16 -0600

    Start of unimplemented language spec

---
+++ spec/language_spec.rb
@@ -0,0 +1,23 @@
+describe "Language" do
+  # Primitives
+  it "should be able to define variables as values"
+  it "should be able to define variables as functions"
+  it "should all multiple expressions in the body of a lambda"
+  it "should be able to quote an expression to prevent evaluation"
+
+  # Math functions
+  it "should have an addition (+) function"
+  it "should have an subtraction (-) function"
+  it "should have an multiplication (*) function"
+  it "should have an division (/) function"
+  it "should have an equality (=) function"
+
+  it "should allow variable arguments to the basic math functions (+, -, *, /)"
+
+  # List functions
+  it "should have a car function"
+  it "should have a first function that works like car"
+  it "should have a cdr function"
+  it "should have a rest function that works like cdr"
+  it "should have a cons function"
+end
