567d1b9356e3dd9f03467fc3240d6d121a5b95a7
Mark A. Nicolosi <mark@nicolosi.me>
2010-03-08 00:12:03 -0600

    Primitive forms are loaded from a runtime file now

---
+++ lib/runtime/primitives.rb
@@ -0,0 +1,34 @@
+require 'lib/environment.rb'
+
+class ScremeForms
+  def self.define(symbol, value = nil, &block)
+    @env ||= Environment.new
+    @env.define(symbol, value, &block)
+  end
+
+  def self.define_special(symbol, &block)
+    @env ||= Environment.new
+    @env.define_special(symbol, &block)
+  end
+
+  def self.env
+    @env
+  end
+end
+
+class Primitives < ScremeForms
+  define_special(:define) do |env, sym, expr|
+    env.define sym, expr.evaluate(env)
+  end
+
+  define_special(:lambda) do |env, formals, body|
+    Function.lambda(env, formals, body)
+  end
+
+  define_special(:quote) do |env, expr|
+    expr
+  end
+
+  define(:+) { |a, b| a + b }
+  define(:*) { |a, b| a * b }
+end
