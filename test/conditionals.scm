383521a15345096e25e3f41d187d774b8133765e
Mark A. Nicolosi <mark@nicolosi.me>
2010-03-24 03:40:36 -0500

    Reformatted context descriptions and added pending (and) unit tests

--- test/conditionals.scm
+++ test/conditionals.scm
@@ -1,18 +1,5 @@
 (context "Conditionals"
-  (context "and"
-    (assert "returns #t if both tests evaluate to #t"
-            (= #t (and (= 2 2) (= 5 5))))
-  )
-  (context "not"
-    (assert "returns #t if argument is #f"
-            (not #f))
-    (assert "returns #f if argument is #t"
-            (not (not #t)))
-    (assert "returns #f for any other argument not #f"
-            (not (and (not 10)
-                      (not 'a))))
-  )
-  (context "if"
+  (context "(if <test> <consequent> <alternate>)"
     (assert "evaluates <consequent> when <test> is a true value"
             (= 10
                (if (= 5 (+ 2 3))
@@ -25,5 +12,20 @@
                       10)))
     (pending "<consequent> and <alternate> are never both evaluated")
     (pending "result is unspecified when <test> is #f and there is no <alternate>")
+  )
+  (context "(and <test1> ...)"
+    (assert "returns #t if both tests evaluate to #t"
+            (= #t (and (= 2 2) (= 5 5))))
+    (pending "returns #t if there are no <test>s")
+    (pending "returns the last <test> if no <test>s are #f")
+  )
+  (context "(not <obj>)"
+    (assert "returns #t if <obj> is #f"
+            (not #f))
+    (assert "returns #f if <obj> is #t"
+            (not (not #t)))
+    (assert "returns #f for any <obj> not #f"
+            (not (and (not 10)
+                      (not 'a))))
   )
 )
